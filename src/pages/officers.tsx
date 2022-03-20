4527aad12c4ee0fb5c7dce0ddf63edbbec8a70af
ehu9480 <86241687+ehu9480@users.noreply.github.com>
2022-03-19 20:35:29 -0400

    Update officers.tsx

--- src/pages/officers.tsx
+++ src/pages/officers.tsx
@@ -11,77 +11,103 @@ export default function Officers() {
 			</Center>
 			<HStack w='97%' spacing={10}>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 			</HStack>
 			<HStack w='97%' spacing={10}>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 				<ImageBox
-					title='Plan Money'
-					desc='The future can be even brighter but a goal without a plan is just a wish'
+					title='Title'
+					name='Name'
 					url='https://bit.ly/dan-abramov'
 					alt='Dan Abramov'
+					desc='This is where we put the information about the officer and 
+						should hopefully show when I hover over their image'
 				/>
 			</HStack>
 		</VStack>
 	);
 }
 
-function ImageBox({ title, desc, url, alt, ...rest}) {
+function ImageBox({ title, name, desc, url, alt, ...rest}) {
+	const[show, setShow] = React.useState(false)
 	return (
-	  <Box
-		mt={2}
-		p={5}
-		shadow='md'
-		borderWidth='1px'
-		flex='1'
-		borderRadius='md'
-		textAlign="center"
-		bg='grey'
-		{...rest}
-	  >
-		<Heading mb={4} fontSize='xl'>{title}</Heading>
-		<Image align='center' objectFit='cover' src={url} alt={alt}/>
-		<Text>{desc}</Text>
-	  </Box>
+		<>
+			<Box
+				role="group"
+				mt={2}
+				p={5}
+				shadow='md'
+				borderWidth='1px'
+				flex='1'
+				borderRadius='md'
+				textAlign="center"
+				bg='grey'
+				//onMouseOver={handleToggle}
+				onMouseEnter={() => setShow(true)}
+				onMouseLeave={() => setShow(false)}
+				{...rest}
+			>
+				<Heading mb={4} fontSize='xl'>{title}</Heading>
+				<Image align='center' objectFit='cover' src={url} alt={alt}/>
+				<Text mt={2} fontSize = 'large'>{name}</Text>
+				{show && (
+					<Text mt={2}> {desc}</Text>
+				)}
+			</Box>
+	  	</>
 	)
-  }
+}
   
